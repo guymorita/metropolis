7feafb4630da1efc1598b4b7a4d80f04acd25416
Guy Morita <guymorita@gmail.com>
2018-08-25 19:44:18 -0400

    Change to ES6

--- src/js/app.js
+++ src/js/app.js
@@ -10,48 +10,48 @@ App = {
   storeIdSelected: null,
   role: null,
 
-  init: function() {
+  init: () => {
     return App.initWeb3()
   },
 
-  initWeb3: function() {
+  initWeb3: () => {
     if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
+      App.web3Provider = web3.currentProvider
     } else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
     }
 
-    web3 = new Web3(App.web3Provider);
+    web3 = new Web3(App.web3Provider)
 
-    return App.initContract();
+    return App.initContract()
   },
 
-  initContract: function() {
-    $.getJSON('Metropolis.json', function(data) {
-      var MetropolisArtifact = data;
-      App.contracts.Metropolis = TruffleContract(MetropolisArtifact);
+  initContract: () => {
+    $.getJSON('Metropolis.json', (data) => {
+      const MetropolisArtifact = data
+      App.contracts.Metropolis = TruffleContract(MetropolisArtifact)
 
-      App.contracts.Metropolis.setProvider(App.web3Provider);
+      App.contracts.Metropolis.setProvider(App.web3Provider)
 
-      web3.eth.getAccounts(function(err, accounts) {
-        App.account = accounts[0];
+      web3.eth.getAccounts((err, accounts) => {
+        App.account = accounts[0]
 
-        App.contracts.Metropolis.deployed().then(function(instance) {
-          App.instance = instance;
+        App.contracts.Metropolis.deployed().then((instance) =>{
+          App.instance = instance
           return App.instance.getRole({ from: App.account })
-        }).then((function(data) {
+        }).then(((data) => {
           App.role = data
 
           App.displayAdminAndOwners()
           return App.getStores()
         }))
       })
-    });
+    })
 
-    return App.bindEvents();
+    return App.bindEvents()
   },
 
-  bindEvents: function() {
+  bindEvents: () => {
     $(document).on('click', '.btn-new-store-name', App.createStore)
     $(document).on('click', '.btn-view-store', App.getItemsWithE)
     $(document).on('click', '.btn-add-item', App.addItem)
@@ -63,23 +63,23 @@ App = {
 
   getStores: () => {
     App.instance.emitStores({ from: App.account })
-      .then(function (data) {
+      .then((data) => {
         const logs = data.logs
         const storeLogs = []
         for (i = 0; i < logs.length; i ++) {
           storeLogs.push(logs[i].args)
         }
         return App.stores = storeLogs
-      }).then(function(stores){
+      }).then((stores) => {
         return App.displayStores()
-      }).catch(function (error) {
-        console.log('error: ', error);
+      }).catch((error) => {
+        console.log('error: ', error)
       })
     return false
   },
 
   getItemsWithE: (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const storeId = Number(e.currentTarget.dataset.id)
     App.getItems(storeId)
     return false
@@ -88,7 +88,7 @@ App = {
   getItems: (storeId) => {
     App.storeIdSelected = storeId
     App.instance.emitItemsWithStoreId(storeId, { from: App.account })
-      .then(function(data) {
+      .then((data) => {
         data.logs[0].args
         const logs = data.logs
         const itemLogs = []
@@ -96,11 +96,11 @@ App = {
           itemLogs.push(logs[i].args)
         }
         return App.items = itemLogs
-      }).then(function() {
+      }).then(() => {
         return App.displayItems()
-      }).catch(function (error) {
-        console.log('error: ', error);
-      });
+      }).catch((error) => {
+        console.log('error: ', error)
+      })
     return false
   },
 
@@ -112,10 +112,10 @@ App = {
     const address = $('.new-store-owner-address').val()
 
     App.instance.createStore(name, address, { from: App.account })
-      .then(function (data) {
-        return App.getStores();
-      }).catch(function (error) {
-        console.log('error: ', error);
+      .then((data) => {
+        return App.getStores()
+      }).catch((error) => {
+        console.log('error: ', error)
       })
     return false
   },
@@ -127,16 +127,16 @@ App = {
     const imgUrl = $('.add-item-img-url').val()
     const priceInEth = $('.add-item-price').val()
 
-    const priceInWei = Number(priceInEth) * WEI_IN_ETH;
+    const priceInWei = Number(priceInEth) * WEI_IN_ETH
 
     App.instance.addItem(Number(storeId), name, imgUrl, priceInWei, { from: App.account })
-      .then(function(data) {
+      .then((data) => {
         alert('successfully added item ' + name)
         App.getItems(storeId)
         return
-      }).catch(function (error) {
-        console.log('error: ', error);
-      });
+      }).catch((error) => {
+        console.log('error: ', error)
+      })
     return false
   },
 
@@ -149,13 +149,13 @@ App = {
     const storeId = App.storeIdSelected
 
     App.instance.buyItem(storeId, itemId, { from: App.account, value: priceInWei })
-      .then(function(data) {
+      .then((data) => {
         alert('successfully purchased item!')
         App.getItems(storeId)
         return
-      }).catch(function (error) {
-        console.log('error: ', error);
-      });
+      }).catch((error) => {
+        console.log('error: ', error)
+      })
     return false
   },
 
@@ -165,12 +165,12 @@ App = {
     const storeId = Number($('.withdraw-balance-store-id').val())
 
     App.instance.withdrawBalance(storeId, { from: App.account, gas: 4000000 })
-      .then(function(data) {
+      .then((data) => {
         alert('successfully withdrew balance!')
         return
-      }).catch(function (error) {
-        console.log('error: ', error);
-      });
+      }).catch((error) => {
+        console.log('error: ', error)
+      })
     return false
   },
 
@@ -193,20 +193,20 @@ App = {
 
   displayStores: () => {
     const stores = App.stores
-    const storesRow = $('#storeRow');
+    const storesRow = $('#storeRow')
     storesRow.empty()
-    const storeTemplate = $('#storeTemplate');
+    const storeTemplate = $('#storeTemplate')
 
     for (i = 0; i < stores.length; i ++) {
       const store = stores[i]
 
-      storeTemplate.find('.panel-title').text(store.name);
-      storeTemplate.find('img').attr('src', store.picture);
-      storeTemplate.find('.store-address').text(store.storeOwner);
-      storeTemplate.find('.store-id').text(store.storeId);
-      storeTemplate.find('.btn-view-store').attr('data-id', store.storeId);
+      storeTemplate.find('.panel-title').text(store.name)
+      storeTemplate.find('img').attr('src', store.picture)
+      storeTemplate.find('.store-address').text(store.storeOwner)
+      storeTemplate.find('.store-id').text(store.storeId)
+      storeTemplate.find('.btn-view-store').attr('data-id', store.storeId)
 
-      storesRow.append(storeTemplate.html());
+      storesRow.append(storeTemplate.html())
     }
     return false
   },
@@ -217,28 +217,32 @@ App = {
     itemsRow.empty()
     itemsRow.show()
     const itemTemplate = $('#itemTemplate')
+    const itemsTitle = $('.items-title')
+    const storeName = App.stores[App.storeIdSelected].name
+    itemsTitle.text('Items at ' + storeName)
+    itemsTitle.show()
 
     for (i = 0; i < items.length; i ++) {
       const item = items[i]
 
-      const priceInEth = item.priceInWei / WEI_IN_ETH;
+      const priceInEth = item.priceInWei / WEI_IN_ETH
 
-      itemTemplate.find('.panel-item-title').text(item.name);
-      itemTemplate.find('img').attr('src', item.imgUrl);
-      itemTemplate.find('.item-owner').text(item.owner);
-      itemTemplate.find('.item-id').text(item.itemId);
-      itemTemplate.find('.item-price').text(priceInEth);
-      itemTemplate.find('.btn-buy-item').attr('data-id', item.itemId);
-      itemTemplate.find('.btn-buy-item').attr('data-price', item.priceInWei);
+      itemTemplate.find('.panel-item-title').text(item.name)
+      itemTemplate.find('img').attr('src', item.imgUrl)
+      itemTemplate.find('.item-owner').text(item.owner)
+      itemTemplate.find('.item-id').text(item.itemId)
+      itemTemplate.find('.item-price').text(priceInEth)
+      itemTemplate.find('.btn-buy-item').attr('data-id', item.itemId)
+      itemTemplate.find('.btn-buy-item').attr('data-price', item.priceInWei)
 
-      itemsRow.append(itemTemplate.html());
+      itemsRow.append(itemTemplate.html())
     }
     return false
   }
-};
+}
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
-});
+$(() => {
+  $(window).load(() => {
+    App.init()
+  })
+})
