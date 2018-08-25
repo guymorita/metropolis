35213e2a5995f6a22968666a8a98bfd275726711
Guy Morita <guymorita@gmail.com>
2018-08-25 19:52:56 -0400

    Switch to port 7545

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,7 @@ App = {
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider
     } else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
     }
 
     web3 = new Web3(App.web3Provider)
